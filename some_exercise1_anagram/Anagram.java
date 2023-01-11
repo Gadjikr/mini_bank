278d65f740487f36028799508f6f2f4faca68ed2
Gadjikr <105589080+Gadjikr@users.noreply.github.com>
2023-01-11 19:30:19 +0300

    Anagram or not anagram ?

---
+++ some_exercise1_anagram/Anagram.java
@@ -0,0 +1,37 @@
+package some_exercise1_anagram;
+
+import java.util.Arrays;
+
+public class Anagram {
+//    String result ;
+
+    public static boolean anagram(String word1, String word2) {
+        if (word1.equals(word2)) {
+            return true;
+        }
+
+        if (word1.length() == word2.length()) {
+            char[] charArr1 = word1.toCharArray();
+            char[] charArr2 = word2.toCharArray();
+            System.out.println(word1 + " -> " + Arrays.toString(charArr1));
+            System.out.println(word2 + " -> " + Arrays.toString(charArr2));
+            System.out.println();
+            Arrays.sort(charArr1);
+            Arrays.sort(charArr2);
+
+            System.out.println(Arrays.toString(charArr1));
+            System.out.println(Arrays.toString(charArr2));
+
+
+            for (int i = 0; i < charArr1.length; i++) {
+                if (charArr1[i] != charArr2[i]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        return false;
+    }
+
+}
---
+++ some_exercise1_anagram/testAnagram.java
@@ -0,0 +1,11 @@
+package some_exercise1_anagram;
+
+public class testAnagram {
+    public static void main(String[] args) {
+
+        System.out.println(Anagram.anagram("kl!!lkfffkat243", "!kat23fffkkll4!"));
+//        System.out.println(Anagram.anagram("kat230", "kat234"));
+//        System.out.println(Anagram.anagram("kat243", "kat234"));
+
+    }
+}
