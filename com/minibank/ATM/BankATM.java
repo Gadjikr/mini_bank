5aeba736e7be830ea6f2e0ac5f3063d61b557937
Gadjikr <105589080+Gadjikr@users.noreply.github.com>
2023-01-27 21:21:57 +0300

    ver 0.5 made client chat made ATM form

---
+++ com/minibank/ATM/BankATM.java
@@ -0,0 +1,89 @@
+package com.minibank.ATM;
+
+import javax.swing.*;
+
+// todo остановился урок 11 время 36.30
+
+public class BankATM {
+    private JPanel mainPanel;
+    private JPanel leftPanel;
+    private JButton checkBalanceButton;
+    private JButton makeDepositButton;
+    private JButton makeWithdrawalButton;
+    private JButton n1Button;
+    private JButton n2Button;
+    private JButton n3Button;
+    private JButton n4Button;
+    private JButton n5Button;
+    private JButton n6Button;
+    private JTextField amountField;
+    private JButton n7Button;
+    private JButton n8Button;
+    private JButton n9Button;
+    private JTextField statusField;
+    private JButton n0Button;
+    private JButton ENTERButton;
+    private JTextArea historyField;
+    private JButton pointButton;
+
+//    public BankATM() {
+//        this.mainPanel = new JPanel();
+//        this.leftPanel = new JPanel();
+//        this.checkBalanceButton = new JButton();
+//        this.makeDepositButton = new JButton();
+//        this.makeWithdrawalButton = new JButton();
+//        this.n1Button = new JButton();
+//        this.n2Button = new JButton();
+//        this.n3Button = new JButton();
+//        this.n4Button = new JButton();
+//        this.n5Button = new JButton();
+//        this.n6Button = new JButton();
+//        this.amountField = new JTextField("", 50);
+//        this.n7Button = new JButton();
+//        this.n8Button = new JButton();
+//        this.n9Button = new JButton();
+//        this.statusField = new JTextField("", 50);
+//        this.n0Button = new JButton();
+//        this.ENTERButton = new JButton();
+//        this.historyField = new JTextArea(10, 50);
+//        this.pointButton = new JButton();
+//    }
+
+    private void createUIComponents() {
+//        JFrame frame = new JFrame("Bank chat");
+        ///////////////////
+//        frame.add(mainPanel);
+//        frame.add(leftPanel);
+//        frame.add(checkBalanceButton);
+//        frame.add(makeDepositButton);
+//        frame.add(makeWithdrawalButton);
+//        frame.add(n1Button);
+//        frame.add(n2Button);
+//        frame.add(n3Button);
+//        frame.add(n4Button);
+//        frame.add(n5Button);
+//        frame.add(n6Button);
+//        frame.add(amountField);
+//        frame.add(n7Button);
+//        frame.add(n8Button);
+//        frame.add(n9Button);
+//        frame.add(statusField);
+//        frame.add(n0Button);
+//        frame.add(ENTERButton);
+//        frame.add(historyField);
+//        frame.add(pointButton);
+
+//        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+//        frame.pack();
+//        frame.setLocationRelativeTo(null);
+//        frame.setVisible(true);
+        /////////////////
+    }
+
+    public static void main(String[] args) {
+//        BankATM bankATM = new BankATM();
+//        bankATM.createUIComponents();
+
+    }
+
+}
--- com/minibank/chat/ChatClient.java
+++ com/minibank/chat/ChatClient.java
@@ -1,9 +1,11 @@
 package com.minibank.chat;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
 import javax.swing.*;
 
-//todo ЗАКОНЧИЛ ТУТ ! УРОК 10 минута 71
 public class ChatClient {
 
     private TextArea output;
@@ -13,7 +15,7 @@ public class ChatClient {
 
     public ChatClient() {
         this.output = new TextArea(10, 50);
-        this.input = new TextField("Message...", 50);
+        this.input = new TextField("", 50);
         this.quitButton = new JButton("Quit");
         this.sendButton = new JButton("Send");
     }
@@ -22,6 +24,7 @@ public class ChatClient {
         JFrame frame = new JFrame("Bank chat");
         frame.setLayout(new BorderLayout());
         frame.add(output, BorderLayout.CENTER);
+        output.setEditable(false);
         frame.add(input, BorderLayout.SOUTH);
 
         JPanel buttonPanel = new JPanel();
@@ -31,8 +34,24 @@ public class ChatClient {
 
         frame.add(buttonPanel, BorderLayout.EAST);
 
+        input.addActionListener(new SendHandler());
+        sendButton.addActionListener(new SendHandler());
+
+        quitButton.addActionListener( (ActionEvent e) ->  {
+                System.exit(0);
+        });
+        // old expression
+//        quitButton.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                System.exit(0);
+//            }
+//        });
+
+
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack();
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
 
@@ -40,6 +59,15 @@ public class ChatClient {
         ChatClient chatClient = new ChatClient();
         chatClient.launchFrame();
 
+    }
+
+    private class SendHandler implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String message = input.getText();
+            output.append(message + "\n");
+            input.setText("");
+        }
     }
 
 }
